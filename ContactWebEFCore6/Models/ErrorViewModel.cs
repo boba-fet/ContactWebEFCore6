0e9a04b8154f1a5c64e07c3bfbfe20c2269cbd24
Boba Fett <mandalorianboba@hotmail.com>
2021-11-04 21:21:33 +0000

    initial commit

---
+++ ContactWebEFCore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactWebEFCore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
