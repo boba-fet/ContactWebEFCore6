8189a5f03157582b506b2cc0e4668f0757f2ad31
Boba Fett <mandalorianboba@hotmail.com>
2021-11-05 03:51:50 +0000

    Updated The EFCorect project to reference new data layer, created new data layer for contact manager data

---
+++ MyContactManagerData/Migrations/20211105034803_add-states-and-contacts.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyContactManagerData.Migrations
+{
+    public partial class addstatesandcontacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Abbreviation = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    PhonePrimary = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PhoneSecondary = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StreetAddress1 = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    StreetAddress2 = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    StateId = table.Column<int>(type: "int", nullable: false),
+                    Zip = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_States_StateId",
+                        column: x => x.StateId,
+                        principalTable: "States",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "States",
+                columns: new[] { "Id", "Abbreviation", "Name" },
+                values: new object[,]
+                {
+                    { 1, "AL", "Alabama" },
+                    { 2, "AK", "Alaska" },
+                    { 3, "AZ", "Arizona" },
+                    { 4, "AR", "Arkansas" },
+                    { 5, "CA", "California" },
+                    { 6, "CO", "Colorado" },
+                    { 7, "CT", "Connecticut" },
+                    { 8, "DE", "Delaware" },
+                    { 9, "DC", "District of Columbia" },
+                    { 10, "FL", "Florida" },
+                    { 11, "GA", "Georgia" },
+                    { 12, "HI", "Hawaii" },
+                    { 13, "ID", "Idaho" },
+                    { 14, "IL", "Illinois" },
+                    { 15, "IN", "Indiana" },
+                    { 16, "IA", "Iowa" },
+                    { 17, "KS", "Kansas" },
+                    { 18, "KY", "Kentucky" },
+                    { 19, "LA", "Louisiana" },
+                    { 20, "ME", "Maine" },
+                    { 21, "MD", "Maryland" },
+                    { 22, "MS", "Massachusetts" },
+                    { 23, "MI", "Michigan" },
+                    { 24, "MN", "Minnesota" },
+                    { 25, "MS", "Mississippi" },
+                    { 26, "MO", "Missouri" },
+                    { 27, "MT", "Montana" },
+                    { 28, "NE", "Nebraska" },
+                    { 29, "NV", "Nevada" },
+                    { 30, "NH", "New Hampshire" },
+                    { 31, "NJ", "New Jersey" },
+                    { 32, "NM", "New Mexico" },
+                    { 33, "NY", "New York" },
+                    { 34, "NC", "North Carolina" },
+                    { 35, "ND", "North Dakota" },
+                    { 36, "OH", "Ohio" },
+                    { 37, "OK", "Oklahoma" },
+                    { 38, "OR", "Oregon" },
+                    { 39, "PA", "Pennsylvania" },
+                    { 40, "RI", "Rhode Island" },
+                    { 41, "SC", "South Carolina" },
+                    { 42, "SD", "South Dakota" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "States",
+                columns: new[] { "Id", "Abbreviation", "Name" },
+                values: new object[,]
+                {
+                    { 43, "TN", "Tennessee" },
+                    { 44, "TX", "Texas" },
+                    { 45, "UT", "Utah" },
+                    { 46, "VT", "Vermont" },
+                    { 47, "VA", "Virginia" },
+                    { 48, "WA", "Washington" },
+                    { 49, "WV", "West Virginia" },
+                    { 50, "WI", "Wisconsin" },
+                    { 51, "WY", "Wyoming" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_StateId",
+                table: "Contacts",
+                column: "StateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "States");
+        }
+    }
+}
---
+++ MyContactManagerData/MyContactManagerDbContext.cs
@@ -0,0 +1,96 @@
+﻿using ContactWebModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MyContactManagerData
+{
+    public class MyContactManagerDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+
+        public DbSet<State> States { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+        public MyContactManagerDbContext()
+        {
+            //purposefully empty: Necessary for Scaffold 
+        }
+
+        public MyContactManagerDbContext(DbContextOptions options)
+            : base(options)
+        {
+            //purposefully empty: sets options appropriatly
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+            var cnstr = _configuration.GetConnectionString("MyContactManager");
+            optionsBuilder.UseSqlServer(cnstr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<State>(x =>
+            {
+                x.HasData(
+                    new State() { Id = 1, Name = "Alabama", Abbreviation = "AL"},
+                    new State() { Id = 2, Name = "Alaska", Abbreviation = "AK" },
+                    new State() { Id = 3, Name = "Arizona", Abbreviation = "AZ" },
+                    new State() { Id = 4, Name = "Arkansas", Abbreviation = "AR" },
+                    new State() { Id = 5, Name = "California", Abbreviation = "CA" },
+                    new State() { Id = 6, Name = "Colorado", Abbreviation = "CO" },
+                    new State() { Id = 7, Name = "Connecticut", Abbreviation = "CT" },
+                    new State() { Id = 8, Name = "Delaware", Abbreviation = "DE" },
+                    new State() { Id = 9, Name = "District of Columbia", Abbreviation = "DC" },
+                    new State() { Id = 10, Name = "Florida", Abbreviation = "FL" },
+                    new State() { Id = 11, Name = "Georgia", Abbreviation = "GA" },
+                    new State() { Id = 12, Name = "Hawaii", Abbreviation = "HI" },
+                    new State() { Id = 13, Name = "Idaho", Abbreviation = "ID" },
+                    new State() { Id = 14, Name = "Illinois", Abbreviation = "IL" },
+                    new State() { Id = 15, Name = "Indiana", Abbreviation = "IN" },
+                    new State() { Id = 16, Name = "Iowa", Abbreviation = "IA" },
+                    new State() { Id = 17, Name = "Kansas", Abbreviation = "KS" },
+                    new State() { Id = 18, Name = "Kentucky", Abbreviation = "KY" },
+                    new State() { Id = 19, Name = "Louisiana", Abbreviation = "LA" },
+                    new State() { Id = 20, Name = "Maine", Abbreviation = "ME" },
+                    new State() { Id = 21, Name = "Maryland", Abbreviation = "MD" },
+                    new State() { Id = 22, Name = "Massachusetts", Abbreviation = "MS" },
+                    new State() { Id = 23, Name = "Michigan", Abbreviation = "MI" },
+                    new State() { Id = 24, Name = "Minnesota", Abbreviation = "MN" },
+                    new State() { Id = 25, Name = "Mississippi", Abbreviation = "MS" },
+                    new State() { Id = 26, Name = "Missouri", Abbreviation = "MO" },
+                    new State() { Id = 27, Name = "Montana", Abbreviation = "MT" },
+                    new State() { Id = 28, Name = "Nebraska", Abbreviation = "NE" },
+                    new State() { Id = 29, Name = "Nevada", Abbreviation = "NV" },
+                    new State() { Id = 30, Name = "New Hampshire", Abbreviation = "NH" },
+                    new State() { Id = 31, Name = "New Jersey", Abbreviation = "NJ" },
+                    new State() { Id = 32, Name = "New Mexico", Abbreviation = "NM" },
+                    new State() { Id = 33, Name = "New York", Abbreviation = "NY" },
+                    new State() { Id = 34, Name = "North Carolina", Abbreviation = "NC" },
+                    new State() { Id = 35, Name = "North Dakota", Abbreviation = "ND" },
+                    new State() { Id = 36, Name = "Ohio", Abbreviation = "OH" },
+                    new State() { Id = 37, Name = "Oklahoma", Abbreviation = "OK" },
+                    new State() { Id = 38, Name = "Oregon", Abbreviation = "OR" },
+                    new State() { Id = 39, Name = "Pennsylvania", Abbreviation = "PA" },
+                    new State() { Id = 40, Name = "Rhode Island", Abbreviation = "RI" },
+                    new State() { Id = 41, Name = "South Carolina", Abbreviation = "SC" },
+                    new State() { Id = 42, Name = "South Dakota", Abbreviation = "SD" },
+                    new State() { Id = 43, Name = "Tennessee", Abbreviation = "TN" },
+                    new State() { Id = 44, Name = "Texas", Abbreviation = "TX" },
+                    new State() { Id = 45, Name = "Utah", Abbreviation = "UT" },
+                    new State() { Id = 46, Name = "Vermont", Abbreviation = "VT" },
+                    new State() { Id = 47, Name = "Virginia", Abbreviation = "VA" },
+                    new State() { Id = 48, Name = "Washington", Abbreviation = "WA" },
+                    new State() { Id = 49, Name = "West Virginia", Abbreviation = "WV" },
+                    new State() { Id = 50, Name = "Wisconsin", Abbreviation = "WI" },
+                    new State() { Id = 51, Name = "Wyoming", Abbreviation = "WY" }
+                );
+            });
+        }
+
+    }
+}
